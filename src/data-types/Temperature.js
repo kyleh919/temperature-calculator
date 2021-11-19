0ad4157ef7cdcc0a177ef10815b4ae2a37e3b4c6
Kyle <kyle.hunter91@gmail.com>
2021-11-19 05:59:25 -0800

    Provides additional support in Temperature data type

--- src/data-types/Temperature.js
+++ src/data-types/Temperature.js
@@ -1,18 +1,31 @@
 import { TEMPERATURE_SCALES } from "../enums/TemperatureScales";
 
-const SCALE_KEY = "scale";
-const VALUE_KEY = "value";
+export const SCALE_KEY = "scale";
+export const VALUE_KEY = "value";
 
+// schema for Temperature = { scale: TEMPERATURE_SCALES, temperature: number }
 export class Temperature {
   /**
-   * @param
+   * @param {Temperature} from
    */
-  toObject() {}
+  static toObject(from) {
+    return {
+      [SCALE_KEY]: from.getScale(),
+      [VALUE_KEY]: from.getValue(),
+    };
+  }
 
   /**
-   * @param
+   * @returns {Temperature}
    */
-  fromObject() {}
+  static fromObject(from) {
+    const to = new Temperature();
+
+    to.setScale(from[SCALE_KEY]);
+    to.setValue(from[VALUE_KEY]);
+
+    return to;
+  }
 
   /** @property {string} */
   scale_ = TEMPERATURE_SCALES.CELSIUS;
@@ -21,7 +34,9 @@ export class Temperature {
   value_ = 0;
 
   /** @return {string} */
-  getScale() {}
+  getScale() {
+    return this.scale_;
+  }
 
   /** @param {string} scale */
   setScale(scale) {
@@ -30,11 +45,19 @@ export class Temperature {
   }
 
   /** @return {number} */
-  getValue() {}
+  getValue() {
+    return this.value_;
+  }
 
   /** @param {number} value */
   setValue(value) {
     this.value_ = value;
     return this;
+  }
+
+  /** @param {Temperature} other */
+  clone(other) {
+    this.scale_ = other.getScale();
+    this.value_ = other.getValue();
   }
 }
