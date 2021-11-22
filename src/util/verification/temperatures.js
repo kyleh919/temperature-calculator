7678b608fb9d64ea944c94d389bfa8598dffcc21
Kyle <kyle.hunter91@gmail.com>
2021-11-22 05:23:24 -0500

    Adds support in isBoiling verification utility Includes isCelsius util

--- src/util/verification/temperatures.js
+++ src/util/verification/temperatures.js
@@ -1,1 +1,18 @@
-export function isBoiling(temperature) {}
+import { Temperature } from "../../data-types/Temperature";
+import { TEMPERATURE_SCALES } from "../../enums/TemperatureScales";
+import { toCelsius } from "../conversions/temperatures";
+
+/** @param {Temperature} temperature */
+function isCelsius(temperature) {
+  return temperature.getScale() === TEMPERATURE_SCALES.CELSIUS;
+}
+
+/** @param {object} currentTemperature */
+export function isBoiling(currentTemperature) {
+  const temperature = Temperature.fromObject(currentTemperature);
+  const celsius = isCelsius(temperature)
+    ? temperature.getValue()
+    : toCelsius(temperature.getValue());
+
+  return celsius > 100;
+}
